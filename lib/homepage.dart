e248f780f30d7bbed69aee0ca500bf5e43d28034
Afif Akromi <massachusetts48@gmail.com>
2022-08-27 23:43:19 +0700

    add text

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clock/clock_view.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,11 +12,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split('.').first;
+    var offsetSign = '';
+
+    if (!timezoneString.startsWith('-')) {
+      offsetSign = '+';
+    }
+
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: const Color(0xFF2D2F41),
-        child: ClockView(),
+      backgroundColor: const Color(0xFF2D2F41),
+      body: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Clock',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  formattedTime,
+                  style: const TextStyle(color: Colors.white, fontSize: 64),
+                ),
+                Text(
+                  formattedDate,
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                ClockView(),
+                const Text(
+                  'Timezone',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: <Widget>[
+                    const Icon(
+                      Icons.language,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      'UTC$offsetSign$timezoneString',
+                      style: const TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
